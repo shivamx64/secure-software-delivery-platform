c282fdb9efb569f54e619c3cbe15c05fc6fceaef
shivamx64 <shivamkumar87148@gmail.com>
2026-07-14 12:34:05 +0530

    minor changes
    Signed-off-by: shivamx64 <shivamkumar87148@gmail.com>

--- app/src/api/metrics.py
+++ app/src/api/metrics.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, Response
 from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
 
-metric_bp = Blueprint("metrics", __name__)
+metrics_bp = Blueprint("metrics", __name__)
 
 @metrics_bp.route("/metrics", methods=["GET"])
 def metrics():
