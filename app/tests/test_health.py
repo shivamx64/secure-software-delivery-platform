1ecbe75d226e43332450d6f56b6103e5123a3eb8
shivamx64 <shivamkumar87148@gmail.com>
2026-07-14 14:02:25 +0530

    test: configure unit tests to use Flask testing configuration
    Signed-off-by: shivamx64 <shivamkumar87148@gmail.com>

--- app/tests/test_health.py
+++ app/tests/test_health.py
@@ -1,12 +1,13 @@
 from src.app import create_app
-from src.config import TestConfig
-
 
 def test_health():
-    app = create_app(TestConfig)
+    app = create_app(testing=True)
 
     client = app.test_client()
 
     response = client.get("/health")
 
     assert response.status_code == 200
+    assert response.get_json() == {
+        "status": "healthy"
+    }
--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -1,12 +1,16 @@
 from src.app import create_app
-from src.config import TestConfig
-
 
 def test_root():
-    app = create_app(TestConfig)
+    app = create_app(testing=True)
 
     client = app.test_client()
 
     response = client.get("/")
 
     assert response.status_code == 200
+
+    assert response.get_json() == {
+        "application": "Secure Software Delivery Platform",
+        "status": "running",
+        "version": "v1",
+    }
